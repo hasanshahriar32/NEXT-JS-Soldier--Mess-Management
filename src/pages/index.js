7f093034769ee2c14142c009636664dbd0140beb
Mr. Hasan <hasanshahriar32@gmail.com>
2023-11-10 00:38:03 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>HI, this is my first next application</h1>
+    </div>
+  );
+}
---
+++ src/pages/user-dashboard.js
@@ -0,0 +1,7 @@
+export default function UserDashboard() {
+  return (
+    <>
+      <h1>Hi, welcome to the user's dashboard</h1>
+    </>
+  );
+}
